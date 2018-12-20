987814bd299dc03a9a5e3e016daf613ff77b75aa
Lloyd Sheng <i@lloydsheng.com>
2018-12-20 15:16:15 +0800

    remove unused code

--- platform/ios/src/MGLMapView.mm
+++ platform/ios/src/MGLMapView.mm
@@ -887,11 +887,9 @@ public:
                          position:(MGLOrnamentPosition)position
                              size:(CGSize)size
                            offset:(CGPoint)offset {
-    UIView *containerView = nil;
     NSMutableArray *updatedConstraints = [NSMutableArray array];
     
     if (@available(iOS 11.0, *)) {
-        containerView = self;
         UILayoutGuide *safeAreaLayoutGuide = self.safeAreaLayoutGuide;
         
         switch (position) {
@@ -925,7 +923,6 @@ public:
         //
         UIViewController *viewController = self.viewControllerForLayoutGuides;
         BOOL useLayoutGuides = viewController.view && viewController.automaticallyAdjustsScrollViewInsets;
-        containerView = useLayoutGuides ? viewController.view : self;
         
         switch (position) {
                 case MGLOrnamentPositionTopLeft:
